a25b56cf7b17c43aca72afdbd5a2898071d10c83
Ben Hass <bhass@zendesk.com>
2015-02-03 11:01:11 -0800

    add movie table view and cell view with title and synopsis

---
+++ MoviesViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MoviesViewController.swift
+//  rottentomatoes
+//
+//  Created by Ben Hass on 2/2/15.
+//  Copyright (c) 2015 benhass. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]! = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        var url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=30&apikey=dagqdghwaq3e3mxyrp7kmmj5")
+
+        var request = NSURLRequest(URL: url!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+        var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
+
+        self.movies = responseDictionary["movies"] as [NSDictionary]
+        self.tableView.reloadData()
+        NSLog("Response: %A", responseDictionary)
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as MovieCell
+
+        var movie = movies[indexPath.row]
+        cell.titleLabel.text = movie["title"] as? String
+        cell.synopsisLabel.text = movie["synopsis"] as? String
+
+        return cell
+    }
+
+}
